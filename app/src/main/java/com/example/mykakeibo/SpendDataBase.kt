77d3466e99ae4937010a98614cb5139a9077a982
Linea319 <acyurio@yahoo.co.jp>
2020-01-31 00:21:35 +0900

    支出の入力準備2

--- app/src/main/java/com/example/mykakeibo/SpendDataBase.kt
+++ app/src/main/java/com/example/mykakeibo/SpendDataBase.kt
@@ -1,11 +1,32 @@
 package com.example.mykakeibo
 
 import androidx.room.*
+import java.time.LocalDate
+
+internal class LocalDateConverter {
+    /**
+     * LocalDate　→　フォーマット文字列
+     * フォーマットはyyyy-MM-dd(デフォルト)
+     * */
+    @TypeConverter
+    fun fromLocalDate(localDate: LocalDate): String {
+        return localDate.toString()
+    }
+
+    /**
+     * フォーマット文字列　→　LocalDate
+     * フォーマットはyyyy-MM-dd(デフォルト)
+     * */
+    @TypeConverter
+    fun toLocalDate(stringDate: String): LocalDate {
+        return LocalDate.parse(stringDate)
+    }
+}
 
 @Entity(tableName = "spends") // usersというテーブルのデータベースであることを表す
 data class SpendData(
     @PrimaryKey val uid: Int, // 主キー ユニークID // 必ず主キーをつける必要がある
-    val dateMillis:Long, //日付(ミリ秒)
+    val dateMillis:LocalDate, //日付
     val purpose: String, // 名目
     val spender: String, // 払った人
     val money: Float, //金額
@@ -18,20 +39,27 @@ interface SpendDao {
     fun getAll(): List<SpendData> // 全件取得のSQLをかく。この時にソートや抽出などもできる。
 
     @Query("SELECT * FROM spends WHERE uid IN (:uniqueIds)")
-    fun loadAllByIds(uniqueIds: IntArray): List<SpendData>// 追加処理　コンフリクトが起こった時は置き換える
+    fun loadAllByIds(uniqueIds: IntArray): List<SpendData>
 
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    fun findByName(first: String, last: String): SpendData
+    @Query("SELECT * FROM spends WHERE purpose LIKE :find_purpose LIMIT 1")
+    fun findByPurpose(find_purpose: String): SpendData
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(spend: SpendData)
 
     @Insert
     fun insertAll(vararg spends: SpendData)
 
     @Delete
     fun delete(spend: SpendData)
+
+    @Query("DELETE FROM spends")
+    fun deleteAll()
 }
 
+//DB
 @Database(entities = arrayOf(SpendData::class), version = 1)
+@TypeConverters(LocalDateConverter::class)
 abstract class SpendDatabase : RoomDatabase() {
     abstract fun spendDao(): SpendDao
 }
